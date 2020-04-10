a3207b8c949486244ad1772982cc776ca96ffbe1
Alex Hahn <alexhahn15@gmail.com>
2020-04-10 12:18:59 -0700

    fix camera zoom to be smooth on exhale

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,14 +6,21 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    private static Vector3 offset = new Vector3(0f, 22.2f, -25.68f);
+    private static Vector3 offset = new Vector3(0f, 25.23f, -40f);
     private Vector3 zoomOffset = Vector3.Scale(offset, new Vector3(0f, 1.2f, 1.7f));
 
     private float speed = 10f;
     private MainBoatController playerScript;
 
+    Camera mainCamera;
+    private int zoom = 75;
+    private int normal = 60;
+    private float smooth = 5f;
+    private bool isZoomed = false;
+
     void Start()
     {
+        mainCamera = Camera.main;
         playerScript = player.GetComponent<MainBoatController>();
     }
 
@@ -22,28 +29,13 @@ public class CameraController : MonoBehaviour
     {
         transform.position = player.transform.position + offset;
 
-        if (playerScript.exhalePhase && playerScript.exhaleIsOn)
+        isZoomed = (playerScript.exhalePhase && playerScript.exhaleIsOn) ? true : false;
+
+        if (isZoomed) 
         {
-            float exhaleDuration = (float)System.Math.Round(playerScript.exhaleDuration, 1);
-
-            if (exhaleDuration < 1)
-            {
-                transform.position = Vector3.Lerp(player.transform.position + offset, player.transform.position + zoomOffset, exhaleDuration);
-            } else {
-                transform.position = Vector3.Lerp(player.transform.position + offset, player.transform.position + zoomOffset, 1);
-            }
-        }
-
-        if (playerScript.inhalePhase && !playerScript.inhaleIsOn)
-        {
-            float breakDuration = (float)System.Math.Round(playerScript.breakDuration, 1);
-
-            if (breakDuration < 1)
-            {
-                transform.position = Vector3.Lerp(player.transform.position + zoomOffset, player.transform.position + offset, breakDuration);
-            } else {
-                transform.position = Vector3.Lerp(player.transform.position + zoomOffset, player.transform.position + offset, 1);
-            }
+            mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, zoom, Time.deltaTime*smooth);
+        } else {
+            mainCamera.fieldOfView = Mathf.Lerp(mainCamera.fieldOfView, normal, Time.deltaTime*smooth);
         }
     }
 }
